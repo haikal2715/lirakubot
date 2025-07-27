ebc5577cad874d52dbe656c672186d554c27093b
haikal2715 <haikalsutanto15@gmail.com>
2025-07-27 22:24:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-# Admin dan lainnya tetap bisa disimpan langsung
-ADMIN_ID = 5715651828
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME")
+GOOGLE_SHEET_CREDENTIAL = os.getenv("GOOGLE_SHEET_CREDENTIAL")
+
+MARGIN_PER_100K = int(os.getenv("MARGIN_PER_100K"))
+BIAYA_ADMIN = int(os.getenv("BIAYA_ADMIN"))
+
+REKENING_BCA = os.getenv("REKENING_BCA")
+IBAN_ADMIN = os.getenv("IBAN_ADMIN")
